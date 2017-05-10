063bdbfdc0052b507aa06fd13894a8493b12dfba
Gergely Németh <ngerle@gmail.com>
2017-05-10 12:20:12 +0200

    Add newline handling

--- data_manager.py
+++ data_manager.py
@@ -1,12 +1,14 @@
 from base64 import b64decode, b64encode
 from datetime import datetime
+from flask import Markup
 
 
-# read the QUESTIONS' file into a @table
 #
-# @file_name: string
-# @table: list of lists of strings
 def get_questiontable_from_file(file_name):
+    '''Read the QUESTIONS' file into a @table.
+
+    @file_name: string
+    @table: list of lists of strings'''
     with open(file_name, "r") as file:
         lines = file.readlines()
     table = [element.replace("\n", "").split(",") for element in lines]
@@ -15,16 +17,16 @@ def get_questiontable_from_file(file_name):
         record[1] = datetime.fromtimestamp(int(record[1])).strftime('%Y-%m-%d %H:%M:%S')
         # BASE64 decode of 5th, 6th and 7th data fields
         record[4] = b64decode(record[4]).decode("utf-8")
-        record[5] = b64decode(record[5]).decode("utf-8")
+        record[5] = Markup(b64decode(record[5]).decode("utf-8").replace("\n", "<br>"))
         record[6] = b64decode(record[6]).decode("utf-8")
     return table
 
 
-# write the QUESTIONS @table into a file
-#
-# @file_name: string
-# @table: list of lists of strings
 def write_questiontable_to_file(file_name, table):
+    '''Write the QUESTIONS @table into a file.
+
+    @file_name: string
+    @table: list of lists of strings'''
     with open(file_name, "w") as file:
         for record in table:
             for record in table:
@@ -38,11 +40,11 @@ def write_questiontable_to_file(file_name, table):
             file.write(row + "\n")
 
 
-# read the ANSWERS file into a @table
-#
-# @file_name: string
-# @table: list of lists of strings
 def get_answertable_from_file(file_name):
+    '''Read the ANSWERS file into a @table.
+
+    @file_name: string
+    @table: list of lists of strings'''
     with open(file_name, "r") as file:
         lines = file.readlines()
     table = [element.replace("\n", "").split(",") for element in lines]
