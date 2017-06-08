d201d0b1bf144cb3f9801e945b273308638e0890
krisztian <alt.krisztian@gmail.com>
2017-06-08 13:59:54 +0200

    Rename the templates, which had inaccurate name

--- main.py
+++ main.py
@@ -30,13 +30,13 @@ def listing():
                            questions=ordered_questions, answer_count_list=answer_count_list)
 
 
-@app.route('/new_question')
+@app.route('/record_form')
 def question_sheet():
     users = data_manager.get_existing_users()
-    return render_template('new_question.html', users=users)
+    return render_template('record_form.html', users=users)
 
 
-@app.route('/new_question/add', methods=['POST'])
+@app.route('/record_form/add', methods=['POST'])
 def add_question():
     table = data_manager.get_questions()
     new_row = []
@@ -52,12 +52,12 @@ def add_question():
     data_manager.write_question_to_db(new_row)
     [[question_id]] = data_manager.query_result(
         """SELECT id FROM question WHERE submission_time = %s""", (time_now,))
-    return redirect(url_for('display_q_and_a', question_id=question_id))
+    return redirect(url_for('question_details', question_id=question_id))
 
 
 @app.route('/question/<question_id>/new_answer', methods=["GET", "POST"])
 @app.route('/question/<question_id>', methods=["GET", "POST"])
-def display_q_and_a(question_id, new_answer=False):
+def question_details(question_id, new_answer=False):
     questions = data_manager.get_questions()
     [question] = [question for question in questions if question[0] == int(question_id)]
     answers_for_question = data_manager.answers_for_question(int(question_id))
@@ -74,11 +74,11 @@ def display_q_and_a(question_id, new_answer=False):
         user_id = request.form['user']
         new_answer_data = [answer_id, time_now, votes, question_id, new_answer_message, image, user_id]
         data_manager.write_answer_to_db(new_answer_data)
-        return redirect(url_for('display_q_and_a', question_id=question_id))
+        return redirect(url_for('question_details', question_id=question_id))
     if request.url.endswith("new_answer"):
         new_answer = True
         users = data_manager.get_existing_users()
-    return render_template("display_question_answers.html", question=question, answers=answers_for_question,
+    return render_template("question_details.html", question=question, answers=answers_for_question,
                            new_answer=new_answer, answer_count=answer_count, tags=tags, comments=comments,
                            users=users)
 
@@ -86,7 +86,7 @@ def display_q_and_a(question_id, new_answer=False):
 @app.route('/question/<question_id>/viewcount')
 def view_counter(question_id):
     data_manager.add_view_number(question_id)
-    return redirect(url_for('display_q_and_a', question_id=question_id))
+    return redirect(url_for('question_details', question_id=question_id))
 
 
 @app.route('/question/<question_id>/delete')
@@ -108,7 +108,7 @@ def add_existing_tag_to_question(question_id, existing_tag_id):
     except Exception as e:
         existing_tags = data_manager.get_existing_tags()
         return render_template('new_tag.html', question_id=question_id, existing_tags=existing_tags, assigned_tag=True)
-    return redirect(url_for('display_q_and_a', question_id=question_id))
+    return redirect(url_for('question_details', question_id=question_id))
 
 
 @app.route('/question/<question_id>/new-tag/add', methods=['POST'])
@@ -120,19 +120,19 @@ def add_new_tag_to_question(question_id):
         return render_template('new_tag.html', question_id=question_id, existing_tags=existing_tags,
                                new_tag_error_message="This tag already exists! Please, type here another one!")
     data_manager.add_new_tag_to_question(question_id, new_tag_name)
-    return redirect(url_for('display_q_and_a', question_id=question_id))
+    return redirect(url_for('question_details', question_id=question_id))
 
 
 @app.route('/question/<question_id>/tag/<tag_id>/delete')
 def delete_tag(question_id, tag_id):
     data_manager.delete_tag(question_id, tag_id)
-    return redirect(url_for('display_q_and_a', question_id=question_id))
+    return redirect(url_for('question_details', question_id=question_id))
 
 
 @app.route('/question/<question_id>/new-comment')
 def add_comment_form(question_id):
     users = data_manager.get_existing_users()
-    return render_template('new_question.html', question_id=question_id, users=users)
+    return render_template('record_form.html', question_id=question_id, users=users)
 
 
 @app.route('/question/<question_id>/new-comment/add', methods=['POST'])
@@ -141,7 +141,7 @@ def add_comment_to_question(question_id):
     message = request.form['comment']
     user_id = request.form['user_id']
     data_manager.add_comment_to_db(question_id, message, submission_time, user_id)
-    return redirect(url_for('display_q_and_a', question_id=question_id))
+    return redirect(url_for('question_details', question_id=question_id))
 
 
 @app.route('/search', methods=['GET', 'POST'])
@@ -166,8 +166,8 @@ def registration():
             data_manager.register_user(row)
             return redirect('/')
         except Exception as e:
-            return render_template('new_question.html', registration=True, user_exists=True)
-    return render_template('new_question.html', registration=True)
+            return render_template('record_form.html', registration=True, user_exists=True)
+    return render_template('record_form.html', registration=True)
 
 
 @app.route('/list_users', methods=['GET', 'POST'])
